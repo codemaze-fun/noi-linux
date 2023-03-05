451af686a5cb39d7314a6b22efb087a1c05fa0e6
Robert Wen <robert@pinclr.com>
2023-03-05 14:00:37 +0800

    update

--- container/test.sh
+++ container/test.sh
@@ -2,4 +2,29 @@
 
 echo "hello"
 
-FILE_TO_CHECK="/test-container.sh"
+# test script
+FILE_TO_CHECK="/test.sh"
+
+test -f "$FILE_TO_CHECK"
+test -x "$FILE_TO_CHECK"
+
+# launch script
+FILE_TO_CHECK="/launch.sh"
+
+test -f "$FILE_TO_CHECK"
+test -x "$FILE_TO_CHECK"
+
+# arbiter
+DIR_TO_CHECK="/usr/local/arbiter"
+test -d "$DIR_TO_CHECK"
+FILE_TO_CHECK="/usr/local/arbiter/local/arbiter_local"
+test -f "$FILE_TO_CHECK"
+test -x "$FILE_TO_CHECK"
+
+# binaries existance check
+BINS_TO_CHECK=("vim" "emacs" "firefox")
+for idx in "${!BINS_TO_CHECK[@]}"
+do
+    #echo ${BINS_TO_CHECK[$idx]}
+    which ${BINS_TO_CHECK[$idx]}
+done
