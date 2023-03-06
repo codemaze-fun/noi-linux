772081b27c2557315fa8fac5697dc9f1c6027fc7
Robert Wen <robert@codemaze.fun>
2023-03-06 13:42:31 +0800

    change resolution of the connected desktop (#28)
    * update banner.txt path
    
    * update path
    
    * add ping
    
    * update
    
    * update
    
    * launch with 2160x1440
    
    * use 16:10

--- container/root/launch.sh
+++ container/root/launch.sh
@@ -11,4 +11,8 @@ xrdp-sesman
 
 # launch x11vnc
 # the xfce4 will launch together with this
-x11vnc -create -forever -rfbauth /etc/x11vnc.pass
+x11vnc -create -shared -repeat -forever \
+        -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 \
+        -env X11VNC_CREATE_GEOM=2560x1440x24 \
+        -gone 'killall Xvfb' \
+        -rfbauth /etc/x11vnc.pass
