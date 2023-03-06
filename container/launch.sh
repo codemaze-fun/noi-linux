06e7b35878c02f0ae31241689c6011faaca7c2d8
Robert Wen <robert@pinclr.com>
2023-03-06 11:01:25 +0800

    launch xrdp-sesman

--- container/launch.sh
+++ container/launch.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# launch the ssh server
 /sbin/sshd
 
+# launch xrdp
 xrdp
 
+# launch xrdp-sesman
+xrdp-sesman
+
+# launch x11vnc
+# the xfce4 will launch together with this
 x11vnc -create -forever -rfbauth /etc/x11vnc.pass
