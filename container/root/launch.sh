1d93cc1178fce148066ed822f2562669dc9eecee
Robert Wen <robert@pinclr.com>
2023-03-07 00:10:46 +0800

    only listen to localhost for security

--- container/root/launch.sh
+++ container/root/launch.sh
@@ -12,5 +12,6 @@ xrdp-sesman
 # launch x11vnc
 # the xfce4 will launch together with this
 x11vnc -create -shared -repeat -forever -rfbauth /etc/x11vnc.pass \
+        -localhost \
         -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 \
         -env X11VNC_CREATE_GEOM=2160x1440x24
