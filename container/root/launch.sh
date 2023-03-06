16bdbdc218e7bacbfd84b1750725b296717461d2
Robert Wen <robert@pinclr.com>
2023-03-06 12:13:58 +0800

    update path

--- container/root/launch.sh
+++ container/root/launch.sh
@@ -11,4 +11,4 @@ xrdp-sesman
 
 # launch x11vnc
 # the xfce4 will launch together with this
-x11vnc -create -forever -rfbauth /etc/x11vnc.pass
+x11vnc -create -forever -rfbauth /etc/x11vnc.pass -geometry 2560x1440
