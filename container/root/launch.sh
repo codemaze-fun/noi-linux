322423de2b49ecf3a35cedb67f5e5e13c5433f88
Robert Wen <robert@pinclr.com>
2023-03-06 13:52:52 +0800

    lower the resolution a bit

--- container/root/launch.sh
+++ container/root/launch.sh
@@ -13,6 +13,6 @@ xrdp-sesman
 # the xfce4 will launch together with this
 x11vnc -create -shared -repeat -forever \
         -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 \
-        -env X11VNC_CREATE_GEOM=2560x1440x24 \
+        -env X11VNC_CREATE_GEOM=2160x1440x24 \
         -gone 'killall Xvfb' \
         -rfbauth /etc/x11vnc.pass
